4d1d7bbbea58a302da110dec0021415a2eebeeb4
Clara <137517398+clarasoaresdev@users.noreply.github.com>
2025-05-07 14:23:01 -0300

    Create CadastroDAO.java

---
+++ src/main/java/org/example/exercicio4/model/dao/CadastroDAO.java
@@ -0,0 +1,10 @@
+package org.example.exercicio4.model.dao;
+
+import org.example.exercicio4.model.CRUD;
+import org.example.exercicio4.model.entities.Cadastro;
+
+public class CadastroDAO implements CRUD {
+
+    public void cadastrar(Cadastro cadastro) {}
+
+}
