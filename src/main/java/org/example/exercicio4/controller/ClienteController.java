350ef7f4378f1b87cf82e9a3b56f305a481e1ba5
Clara <137517398+clarasoaresdev@users.noreply.github.com>
2025-05-07 14:16:20 -0300

    adicionando ClienteController.java

---
+++ src/main/java/org/example/exercicio4/controller/ClienteController.java
@@ -0,0 +1,9 @@
+package org.example.exercicio4.controller;
+
+import org.example.exercicio4.model.CRUD;
+
+public class ClienteController implements CRUD {
+
+    @Override
+    public void cadastrar() {}
+}
