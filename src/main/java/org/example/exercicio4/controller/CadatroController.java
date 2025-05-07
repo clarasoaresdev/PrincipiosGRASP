6524e940d4c1d566a3502e4a64418d18e7d85e55
Clara <137517398+clarasoaresdev@users.noreply.github.com>
2025-05-07 14:15:41 -0300

    adicionando CadatroController.java

---
+++ src/main/java/org/example/exercicio4/controller/CadatroController.java
@@ -0,0 +1,8 @@
+package org.example.exercicio4.controller;
+
+import org.example.exercicio4.model.CRUD;
+
+public class CadastroController implements CRUD {
+    @Override
+    public void cadastrar() {}
+}
